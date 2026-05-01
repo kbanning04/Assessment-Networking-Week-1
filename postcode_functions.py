08f73fae4056073ca92ef13fe117522ab469c082
kbanning04 <kbanning04@gmail.com>
2026-05-01 14:33:15 +0100

    Validating postcodes

--- postcode_functions.py
+++ postcode_functions.py
@@ -6,6 +6,9 @@ import json
 
 CACHE_FILE = "./postcode_cache.json"
 
+POSTCODE_URL = "https://api.postcodes.io/postcodes"
+
+
 def load_cache() -> dict:
     """Loads the cache from a file and converts it from JSON to a dictionary."""
     # This function is used in Task 3, you can ignore it for now.
@@ -18,13 +21,36 @@ def save_cache(cache: dict):
     ...
 
 
-
 def validate_postcode(postcode: str) -> bool:
-    pass
+    """
+    Returns if an inputted postcode is valid or not.
+    """
+    if not isinstance(postcode, str):
+        raise TypeError("Function expects a string.")
+    response = req.get(
+        f"{POSTCODE_URL}/{postcode}/validate")
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response["result"]
+    else:
+        if response.status_code == 500:
+            raise req.RequestException("Unable to access API.")
+        return False
 
 
 def get_postcode_for_location(lat: float, long: float) -> str:
-    pass
+    """
+    Returns the postcode of an inputted latitude and longitude.
+    """
+    if not isinstance(long, float) or not isinstance(lat, float):
+        raise TypeError("Function expects two floats.")
+    response = req.get(f"{POSTCODE_URL}?lon={long}&lat={lat}")
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response["result"][0]["admin_district"]
+    else:
+        if response.status_code == 500:
+            raise req.RequestException("Unable to access API.")
 
 
 def get_postcode_completions(postcode_start: str) -> list[str]:
